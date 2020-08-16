6e4fd158d7f76892e5f7da26b610b3e2de928246
Lucas <lucashenriqueos@hotmail.com>
2020-08-16 15:49:45 -0300

    Adding RoomDB

---
+++ data/src/main/java/org/lucashos/data/db/AppDatabase.kt
@@ -0,0 +1,11 @@
+package org.lucashos.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import org.lucashos.data.db.dao.MovieDao
+import org.lucashos.data.domain.entity.MovieEntity
+
+@Database(entities = arrayOf(MovieEntity::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+}
