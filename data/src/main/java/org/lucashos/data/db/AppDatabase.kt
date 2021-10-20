5dacd80a6ad465822da5cd70dfcff20138b84c95
Lucas Henrique <lucashenriqueos92@gmail.com>
2021-10-20 12:04:16 -0300

    [IMPROVEMENT] - Improving dagger dependency injection (#14)
    * improving dependency injection
    
    * improving ci triggers
    
    * improving ci triggers
    
    Co-authored-by: Lucas Henrique <lucas.henrique@neon.com.br>

--- data/src/main/java/org/lucashos/data/db/AppDatabase.kt
+++ data/src/main/java/org/lucashos/data/db/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import org.lucashos.data.db.dao.MovieDao
 import org.lucashos.data.domain.entity.MovieEntity
 
-@Database(entities = arrayOf(MovieEntity::class), version = 1)
+@Database(entities = [MovieEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
 }
--- data/src/main/java/org/lucashos/data/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/org/lucashos/data/repository/MovieRepositoryImpl.kt
@@ -1,8 +1,7 @@
 package org.lucashos.data.repository
 
 import io.reactivex.Single
-import org.lucashos.core.api.ApiClient
-import org.lucashos.data.db.AppDatabase
+import org.lucashos.data.db.dao.MovieDao
 import org.lucashos.data.domain.entity.MovieEntity
 import org.lucashos.data.service.TmdbApiService
 import org.lucashos.domain.entity.GenreBO
@@ -10,47 +9,40 @@ import org.lucashos.domain.entity.MovieBO
 import org.lucashos.domain.entity.MovieDetailBO
 import org.lucashos.domain.entity.MoviesListBO
 import org.lucashos.domain.repository.MovieRepository
+import javax.inject.Inject
 
-class MovieRepositoryImpl(private val apiClient: ApiClient, private val appDatabase: AppDatabase) : MovieRepository {
+class MovieRepositoryImpl @Inject constructor(
+    private val service: TmdbApiService,
+    private val dao: MovieDao
+) : MovieRepository {
 
-    override fun listTopMovies(page: Int): Single<MoviesListBO> =
-        apiClient.retrofit.create(TmdbApiService::class.java)
-            .getTopMovies(page).map { it.toBO() }
+    override fun listTopMovies(page: Int): Single<MoviesListBO> = service.getTopMovies(page).map { it.toBO() }
 
-    override fun searchMovies(title: String, page: Int): Single<MoviesListBO> =
-        apiClient.retrofit.create(TmdbApiService::class.java)
-            .searchMovies(title, page).map { it.toBO() }
+    override fun searchMovies(title: String, page: Int): Single<MoviesListBO> = service.searchMovies(title, page)
+        .map { it.toBO() }
 
-    override fun getSimilarMovies(id: Long): Single<MoviesListBO> =
-        apiClient.retrofit.create(TmdbApiService::class.java)
-            .getSimilarMovies(id).map { it.toBO() }
+    override fun getSimilarMovies(id: Long): Single<MoviesListBO> = service.getSimilarMovies(id).map { it.toBO() }
 
-    override fun getMovieDetail(id: Long): Single<MovieDetailBO> =
-        apiClient.retrofit.create(TmdbApiService::class.java)
-            .getMovie(id).map { it.toBO() }
+    override fun getMovieDetail(id: Long): Single<MovieDetailBO> = service.getMovie(id).map { it.toBO() }
 
     override fun getRandomPopularMovie(): Single<MovieBO> =
-        apiClient.retrofit.create(TmdbApiService::class.java)
-            .getPopularMovies()
+        service.getPopularMovies()
             .map { it.movies.random() }
             .map { it.toBO() }
 
     override fun getGenres(): Single<List<GenreBO>> =
-        apiClient.retrofit.create(TmdbApiService::class.java)
-            .getGenres()
+        service.getGenres()
             .map {
                 it.genres.map { genre ->
                     genre.toBO()
                 }
             }
 
-    override fun findFavourite(id: Long): Single<Int> =
-        appDatabase.movieDao().findFavourite(id)
+    override fun findFavourite(id: Long): Single<Int> = dao.findFavourite(id)
 
-    override fun addFavourite(id: Long): Single<Any> =
-        appDatabase.movieDao().insert(MovieEntity(id)).toSingleDefault(true)
+    override fun addFavourite(id: Long): Single<Any> = dao.insert(MovieEntity(id)).toSingleDefault(true)
 
-    override fun removeFavourite(id: Long): Single<Int> = appDatabase.movieDao().delete(MovieEntity(id))
+    override fun removeFavourite(id: Long): Single<Int> = dao.delete(MovieEntity(id))
 
 
 }
